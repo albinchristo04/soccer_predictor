de2fe25819ab94e1d413cc4e518c95107504af85
Roni Altshuler <roni.altshuler@gmail.com>
2025-11-03 22:29:30 +0200

    Fix: Wrap PageLoader in Suspense boundary for useSearchParams()

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { Navbar } from '@/components/Navbar'
 import { Footer } from '@/components/Footer'
 import { PageLoader } from '@/components/PageLoader'
@@ -14,7 +15,9 @@ export default function RootLayout({
         <link rel="icon" href="/soccer-ball.png" />
       </head>
       <body className="min-h-screen">
-        <PageLoader />
+        <Suspense fallback={null}>
+          <PageLoader />
+        </Suspense>
         <div className="flex flex-col min-h-screen">
           <Navbar />
           <main className="flex-grow container mx-auto px-4 py-8">
